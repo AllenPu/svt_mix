66bf0be35014acdef2ab76dabae0e3740c0ea61c
AllenPu <18651885620@163.com>
2022-07-14 19:32:57 -0400

    change the image mixup to the video spatial

--- utils/mixup.py
+++ utils/mixup.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from timm.data import Mixup
+#from timm.data import Mixup
 import torch.nn.functional as F
 
 def rand_bbox(img_shape, lam, margin=0., count=None):
@@ -32,15 +32,23 @@ def data_mixup(x, lam):
     # x in shape of : [B, C, T, H, W]
     # lam is the mixup ratio
     #
+    # in our desired shape
+    assert len(x.shape) == 5
+    temporal_len = x.shape[2]
+    #
     if lam is None:
         lam = np.random.beta(1., 1., size=[x.shape[0],1,1,1]).astype(np.float32)
         lam = torch.from_numpy(lam).cuda()
     new_lams = lam * 0.8 + 0.1
     x_flip = x.flip(0).clone()
     for i in range(x.shape[0]):
-        yl, yh, xl, xh = rand_bbox(x.shape, new_lams[i].cpu())
+        # two video mixup in a temproal level       
+        x_image = x[:, :, 0, :, :]
+        yl, yh, xl, xh = rand_bbox(x_image.shape, new_lams[i].cpu())
         bbox_area = (yh - yl) * (xh - xl)
-        new_lams[i] = torch.tensor(1. - bbox_area / float(x.shape[-2] * x.shape[-1])).cuda()
-        x[i:i+1, :, yl:yh, xl:xh] = F.interpolate(x_flip[i:i+1], (yh - yl, xh - xl), mode="bilinear")
+        new_lams[i] = torch.tensor(1. - bbox_area / float(x_image.shape[-2] * x_image.shape[-1])).cuda()
+        # mix up for two videos frame by frame
+        for j in range(temporal_len):
+            x[i:i+1, :, j, yl:yh, xl:xh] = F.interpolate(x_flip[i:i+1, :, j, :, :], (yh - yl, xh - xl), mode="bilinear")
     # mixuped x, original ratio (first derievd from the beta distribution), new ratio after mixed up
     return x, lam, new_lams
